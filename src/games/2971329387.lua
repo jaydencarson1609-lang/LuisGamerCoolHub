a826f5437bd143d961457d3fab3372bf31823b78
jaydencarson1609-lang <jaydencarson1609@gmail.com>
2026-07-11 20:38:04 +0100

    Update 2971329387.lua

--- src/games/2971329387.lua
+++ src/games/2971329387.lua
@@ -3,11 +3,15 @@ src/games/2971329387.lua — Cook Burgers
 LuisGamerCoolHub
 ]]
 
-return function(ContentHolder, api)
-    api.Tab("main", function(tab)
-        tab.Button("spawn plate", function()
-            local Event = workspace.Restaurant.PlateDispenser.DispenserButton.ContextAction
-            Event:FireServer()
+return function(_, api)
+    api.Tab("Main", function(tab)
+        tab.Button("Spawn Plate", function()
+            local restaurant = workspace:WaitForChild("Restaurant")
+            local dispenser = restaurant:WaitForChild("PlateDispenser")
+            local button = dispenser:WaitForChild("DispenserButton")
+            local event = button:WaitForChild("ContextAction")
+
+            event:FireServer()
         end)
     end)
 end
